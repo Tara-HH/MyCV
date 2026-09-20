98a85e48f986a6d57336591cdd01d5f6155156e7
Tara Hajihasani <Tarahajihasani@gmail.com>
2026-09-20 21:54:55 +0330

    feat: add URL routing and views for apps

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+def about(request):
+    return render(request, 'core/about.html')
+def resume(request):
+    return render(request, 'core/resume.html')
+def services(request):
+    return render(request, 'core/services.html')
